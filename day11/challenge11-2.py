6e2fa7dde4a8843a6ba3e6cb39c584c03d3c76b8
ThePyromancer23 <thepyromancer23@gmail.com>
2023-12-11 09:03:58 -0600

    Day 11

--- day11/challenge11-2.py
+++ day11/challenge11-2.py
@@ -1,25 +1,17 @@
-from copy import deepcopy
 map = []
 
 def expand(map):
-    new_map = deepcopy(map)
-
-    insertions = []
+    row_expansion = []
     for i in range(len(map)):
         if all(x == '.' for x in map[i]):
-            insertions.append(i)
-    for i in range(len(insertions)):
-        new_map.insert(insertions[i] + i, ['.']*len(map[0]))
+            row_expansion.append(i)
 
-    insertions = []
+    column_expansion = []
     for j in range(len(map[0])):
         if all(map[i][j] == '.' for i in range(len(map))):
-            insertions.append(j)
-    for j in range(len(insertions)):
-        for i in range(len(new_map)):
-            new_map[i].insert(insertions[j]+j, '.')
+            column_expansion.append(j)
     
-    return new_map
+    return row_expansion, column_expansion
 
 def find_galaxies(map):
     coords = []
@@ -38,7 +30,7 @@ for line in lines:
     temp = line.replace('\n','')
     map.append([*temp])
 
-map = expand(map)
+row_exp, col_exp = expand(map)
 
 coords = find_galaxies(map)
 
@@ -48,6 +40,15 @@ for i in range(len(coords)):
     for j in range(i+1, len(coords)):
         x1, y1 = coords[i]
         x2, y2 = coords[j]
-        sum += (abs(x1 - x2) + abs(y1 - y2))
+
+        exp = 0
+        for row in row_exp:
+            if min(x1, x2) <= row <= max(x1, x2):
+                exp += 999999
+        for col in col_exp:
+            if min(y1, y2) <= col <= max(y1, y2):
+                exp += 999999
+    
+        sum += (abs(x1 - x2)) + abs(y1 - y2) + exp
 
 print("Sum of shorted paths between each galaxy:", sum)
